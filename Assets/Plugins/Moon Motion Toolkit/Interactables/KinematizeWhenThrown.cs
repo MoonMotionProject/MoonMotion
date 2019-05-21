63c7b4d0aabf218b2781f2f79c0752066617cf43
Hunter-Bobeck <hdbipod@gmail.com>
2019-05-21 17:19:07 -0400

    v.2.4.0
    • added Kinematize When Thrown

---
+++ Assets/Plugins/Moon Motion Toolkit/Interactables/KinematizeWhenThrown.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Kinematize When Thrown:
+// • kinematizes this object's rigidbody as soon as it is thrown
+namespace Valve.VR.InteractionSystem
+{
+	[RequireComponent(typeof(Interactable))]
+	[RequireComponent(typeof(Rigidbody))]
+	public class KinematizeWhenThrown : MonoBehaviour
+	{
+		// variables //
+
+
+		private new Rigidbody rigidbody;        // connection - automatic: this object's rigidbody
+
+
+
+
+		// updating //
+
+		
+		// before the start: //
+		private void Awake()
+		{
+			// connect to this object's rigidbody //
+			rigidbody = GetComponent<Rigidbody>();
+		}
+
+		// Interactable events //
+		private void OnDetachedFromHand(Hand hand)        // "Called every Update() while this GameObject is attached to the hand"
+		{
+			rigidbody.isKinematic = true;
+		}
+	}
+}

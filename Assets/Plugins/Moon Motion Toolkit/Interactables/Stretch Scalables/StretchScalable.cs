b9d2939a23b7d6726c4371b99091154ab7b073d6
Hunter-Bobeck <hdbipod@gmail.com>
2019-05-28 21:45:36 -0400

    v.2.7.0
    • added an instance boolean 'stretchScaling' to Stretch Scalable to provide whether stretch scaling is currently occurring via that component instance
    • provided default values for the min and max vectors of Stretch Scalable

--- Assets/Plugins/Moon Motion Toolkit/Interactables/Stretch Scalables/StretchScalable.cs
+++ Assets/Plugins/Moon Motion Toolkit/Interactables/Stretch Scalables/StretchScalable.cs
@@ -16,15 +16,16 @@ namespace Valve.VR.InteractionSystem
 		[Tooltip("the controller operation during which to consider this object stretched (if it is already held)")]
 		public ControllerOperation stretchingOperation;
 		[Tooltip("the min world scale for each axis (other than the hard minimum of zero for each axis) that this object can be stretched to by this component")]
-		public Vector3 worldScaleMin;
+		public Vector3 worldScaleMin = new Vector3(.1f, .1f, .1f);
 		[Tooltip("the max world scale for each axis that this object can be stretched to by this component")]
-		public Vector3 worldScaleMax;
+		public Vector3 worldScaleMax = new Vector3(100f, 100f, 100f);
 
 		// trackings //
 		private Controller stretchingController;        // the last controller to be used for stretching
 		private Vector3 previousWorldScale;        // the previous world scale of this object, tracked before starting to stretch
 		private Vector3 stretchingControllerStartPosition;     // the position upon starting the last stretch of the last controller to be used for stretching
 		private float startingControllersDistance;		// the distance between the controllers upon last starting stretching
+		[HideInInspector] public bool stretchScaling = false;       // whether stretch scaling is currently occurring via this component
 
 
 
@@ -32,15 +33,30 @@ namespace Valve.VR.InteractionSystem
 		// updating //
 
 
+		// upon validation: //
+		private void OnValidate()
+		{
+			if (GetComponent<Throwable>())
+			{
+				GetComponent<Throwable>().swappingEnabled = false;
+			}
+		}
+
 		// Interactable events //
 		private void OnAttachedToHand(Hand hand)       // "Called when this GameObject becomes attached to the hand"
 		{
 			stretchingController = hand.GetComponent<Controller>().otherController;
 		}
+		private void OnDetachedFromHand(Hand hand)        // "Called every Update() while this GameObject is attached to the hand"
+		{
+			stretchScaling = false;
+		}
 		private void HandAttachedUpdate(Hand hand)        // "Called every Update() while this GameObject is attached to the hand"
 		{
 			if (stretchingOperation.operated(StatesOfBeing.Beingness.becoming))
 			{
+				stretchScaling = true;
+
 				transform.parent = null;
 				previousWorldScale = transform.localScale;
 				transform.parent = hand.transform;
@@ -50,6 +66,8 @@ namespace Valve.VR.InteractionSystem
 			}
 			if (stretchingOperation.operated(StatesOfBeing.Beingness.being))
 			{
+				stretchScaling = true;
+
 				float controllersDistance = Vector3.Distance(stretchingController.transform.position, hand.transform.position);
 				float stretchDistance = controllersDistance - startingControllersDistance;
 
@@ -61,6 +79,10 @@ namespace Valve.VR.InteractionSystem
 				transform.parent = null;
 				transform.localScale = Vector3.Max(Vector3.zero, Vector3.Max(worldScaleMin, Vector3.Min(worldScaleMax, targetWorldScale)));
 				transform.parent = hand.transform;
+			}
+			if (stretchingOperation.operated(StatesOfBeing.Beingness.unbecoming))
+			{
+				stretchScaling = false;
 			}
 		}
 	}

a9a97c77ea7678457f3c70acdb25a412e5249283
Hunter-Bobeck <hdbipod@gmail.com>
2019-09-18 00:26:06 -0400

    v.10.8.0
    • improved RadialCollisionExtensions
    • added PositionalCollisionExtensions

---
+++ Assets/Plugins/Moon Motion Toolkit/Utilities/Extensions/PositionalCollisionExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Positional Collision Extensions: provides extension methods for handling positional collisions //
+public static class PositionalCollisionExtensions
+{
+	// methods: return the set of colliders\objects\rigidbodies (respectively) at this given position provider, using the given layer mask and trigger collider query //
+
+	
+	public static HashSet<Collider> positionalColliders(this Vector3 centerPosition, QueryTriggerInteraction triggerColliderQuery = Default.radialTriggerColliderQuery, params LayerMask[] layerMask_MaxOf1)
+		=>	centerPosition.collidersWithin
+			(
+				0f,
+				triggerColliderQuery,
+				layerMask_MaxOf1
+			);
+
+	public static HashSet<Collider> positionalColliders(this Component centerComponent, QueryTriggerInteraction triggerColliderQuery = Default.radialTriggerColliderQuery, params LayerMask[] layerMask_MaxOf1)
+		=> centerComponent.position().positionalColliders(triggerColliderQuery, layerMask_MaxOf1);
+
+	public static HashSet<Collider> positionalColliders(this GameObject centerObject, QueryTriggerInteraction triggerColliderQuery = Default.radialTriggerColliderQuery, params LayerMask[] layerMask_MaxOf1)
+		=> centerObject.position().positionalColliders(triggerColliderQuery, layerMask_MaxOf1);
+
+
+	public static HashSet<GameObject> positionalObjects(this Vector3 centerPosition, QueryTriggerInteraction triggerColliderQuery = Default.radialTriggerColliderQuery, params LayerMask[] layerMask_MaxOf1)
+		=>	centerPosition.positionalColliders
+			(
+				triggerColliderQuery,
+				layerMask_MaxOf1
+			).uniqueObjects();
+
+	public static HashSet<GameObject> positionalObjects(this Component centerComponent, QueryTriggerInteraction triggerColliderQuery = Default.radialTriggerColliderQuery, params LayerMask[] layerMask_MaxOf1)
+		=> centerComponent.position().positionalObjects(triggerColliderQuery, layerMask_MaxOf1);
+
+	public static HashSet<GameObject> positionalObjects(this GameObject centerObject, QueryTriggerInteraction triggerColliderQuery = Default.radialTriggerColliderQuery, params LayerMask[] layerMask_MaxOf1)
+		=> centerObject.position().positionalObjects(triggerColliderQuery, layerMask_MaxOf1);
+
+
+	public static HashSet<Rigidbody> positionalRigidbodies(this Vector3 centerPosition, QueryTriggerInteraction triggerColliderQuery = Default.radialTriggerColliderQuery, params LayerMask[] layerMask_MaxOf1)
+		=>	centerPosition.positionalColliders
+			(
+				triggerColliderQuery,
+				layerMask_MaxOf1
+			).uniqueRigidbodies();
+
+	public static HashSet<Rigidbody> positionalRigidbodies(this Component centerComponent, QueryTriggerInteraction triggerColliderQuery = Default.radialTriggerColliderQuery, params LayerMask[] layerMask_MaxOf1)
+		=> centerComponent.position().positionalRigidbodies(triggerColliderQuery, layerMask_MaxOf1);
+
+	public static HashSet<Rigidbody> positionalRigidbodies(this GameObject centerObject, QueryTriggerInteraction triggerColliderQuery = Default.radialTriggerColliderQuery, params LayerMask[] layerMask_MaxOf1)
+		=> centerObject.position().positionalRigidbodies(triggerColliderQuery, layerMask_MaxOf1);
+}
--- Assets/Plugins/Moon Motion Toolkit/Utilities/Extensions/RadialCollisionExtensions.cs
+++ Assets/Plugins/Moon Motion Toolkit/Utilities/Extensions/RadialCollisionExtensions.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 // Radial Collision Extensions: provides extension methods for handling radial collisions //
 public static class RadialCollisionExtensions
 {
-	// methods: return the set of colliders\objects\rigidbodies (respectively) within the given radius of this given center position, using the given layer mask and trigger collider query //
+	// methods: return the set of colliders\objects\rigidbodies (respectively) within the given radius of this given center position provider, using the given layer mask and trigger collider query //
 
+	
 	public static HashSet<Collider> collidersWithin(this Vector3 centerPosition, float radius, QueryTriggerInteraction triggerColliderQuery = Default.radialTriggerColliderQuery, params LayerMask[] layerMask_MaxOf1)
 		=>	Physics.OverlapSphere
 			(
@@ -15,7 +16,14 @@ public static class RadialCollisionExtensions
 				layerMask_MaxOf1.firstOtherwise(Default.layerMask),
 				triggerColliderQuery
 			).toSet();
-	
+
+	public static HashSet<Collider> collidersWithin(this Component centerComponent, float radius, QueryTriggerInteraction triggerColliderQuery = Default.radialTriggerColliderQuery, params LayerMask[] layerMask_MaxOf1)
+		=> centerComponent.position().collidersWithin(radius, triggerColliderQuery, layerMask_MaxOf1);
+
+	public static HashSet<Collider> collidersWithin(this GameObject centerObject, float radius, QueryTriggerInteraction triggerColliderQuery = Default.radialTriggerColliderQuery, params LayerMask[] layerMask_MaxOf1)
+		=> centerObject.position().collidersWithin(radius, triggerColliderQuery, layerMask_MaxOf1);
+
+
 	public static HashSet<GameObject> objectsWithin(this Vector3 centerPosition, float radius, QueryTriggerInteraction triggerColliderQuery = Default.radialTriggerColliderQuery, params LayerMask[] layerMask_MaxOf1)
 		=>	centerPosition.collidersWithin
 			(
@@ -23,12 +31,25 @@ public static class RadialCollisionExtensions
 				triggerColliderQuery,
 				layerMask_MaxOf1
 			).uniqueObjects();
-	
+
+	public static HashSet<GameObject> objectsWithin(this Component centerComponent, float radius, QueryTriggerInteraction triggerColliderQuery = Default.radialTriggerColliderQuery, params LayerMask[] layerMask_MaxOf1)
+		=> centerComponent.position().objectsWithin(radius, triggerColliderQuery, layerMask_MaxOf1);
+
+	public static HashSet<GameObject> objectsWithin(this GameObject centerObject, float radius, QueryTriggerInteraction triggerColliderQuery = Default.radialTriggerColliderQuery, params LayerMask[] layerMask_MaxOf1)
+		=> centerObject.position().objectsWithin(radius, triggerColliderQuery, layerMask_MaxOf1);
+
+
 	public static HashSet<Rigidbody> rigidbodiesWithin(this Vector3 centerPosition, float radius, QueryTriggerInteraction triggerColliderQuery = Default.radialTriggerColliderQuery, params LayerMask[] layerMask_MaxOf1)
 		=>	centerPosition.collidersWithin
 			(
 				radius,
 				triggerColliderQuery,
-				layerMask_MaxOf1.firstOtherwise(Default.layerMask)
+				layerMask_MaxOf1
 			).uniqueRigidbodies();
+
+	public static HashSet<Rigidbody> rigidbodiesWithin(this Component centerComponent, float radius, QueryTriggerInteraction triggerColliderQuery = Default.radialTriggerColliderQuery, params LayerMask[] layerMask_MaxOf1)
+		=> centerComponent.position().rigidbodiesWithin(radius, triggerColliderQuery, layerMask_MaxOf1);
+
+	public static HashSet<Rigidbody> rigidbodiesWithin(this GameObject centerObject, float radius, QueryTriggerInteraction triggerColliderQuery = Default.radialTriggerColliderQuery, params LayerMask[] layerMask_MaxOf1)
+		=> centerObject.position().rigidbodiesWithin(radius, triggerColliderQuery, layerMask_MaxOf1);
 }

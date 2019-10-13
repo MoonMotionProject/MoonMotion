fe227d82de37bbaa0422ee2089cdc25a77dbe2f7
Hunter-Bobeck <hdbipod@gmail.com>
2019-10-13 00:52:06 -0400

    v.11.15.0
    • added dashing and sweeping requisites
    • updated Player prefab to consider these requisites for boosting and treading

---
+++ Assets/Plugins/Moon Motion Toolkit/Dependencies/Requisites/DashingRequisite.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dashing", menuName = "Moon Motion/Dependency Requisites/Dashing")]
+public class DashingRequisite : DependencyRequisite
+{
+	public override bool state => Dashing.currentlyDashing;
+}
---
+++ Assets/Plugins/Moon Motion Toolkit/Dependencies/Requisites/SweepingRequisite.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Sweeping", menuName = "Moon Motion/Dependency Requisites/Sweeping")]
+public class SweepingRequisite : DependencyRequisite
+{
+	public override bool state => Sweeping.currentlySweeping;
+}

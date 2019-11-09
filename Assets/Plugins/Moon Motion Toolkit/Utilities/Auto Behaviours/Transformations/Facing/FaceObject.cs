db64aff208766ab180f900136631d5b8e3085447
Hunter-Bobeck <hdbipod@gmail.com>
2019-11-08 23:07:40 -0500

    v.16.0.2
    • fixed bad attribute

--- Assets/Plugins/Moon Motion Toolkit/Utilities/Auto Behaviours/Transformations/Facing/FaceObject.cs
+++ Assets/Plugins/Moon Motion Toolkit/Utilities/Auto Behaviours/Transformations/Facing/FaceObject.cs
@@ -30,8 +30,6 @@ public class FaceObject : AutoBehaviour<FaceObject>
 	[InlineButton("targetCamera", "Target Camera")]
 	[HideLabel]
 	[ShowInInspector]
-	#else
-	[ShowAssetPreview]
 	#endif
 	public GameObject targetObject
 	{

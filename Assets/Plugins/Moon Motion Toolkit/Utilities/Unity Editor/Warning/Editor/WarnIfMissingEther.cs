b61d4479c678960c6ea5a45143c0c164e7805c30
Hunter-Bobeck <hdbipod@gmail.com>
2019-11-10 00:36:16 -0500

    v.18.1.0
    • a warning is now printed if no Ether exists in the hierarchy
    • updated New Moon Motion Project Scene to include Ether and Player

---
+++ Assets/Plugins/Moon Motion Toolkit/Utilities/Unity Editor/Warning/Editor/WarnIfMissingEther.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class WarnIfMissingEther
+{
+	static WarnIfMissingEther()
+	{
+		potentiallyWarn();
+		EditorApplication.playModeStateChanged += potentiallyWarn;
+	}
+
+	private static void potentiallyWarn()
+	{
+		if (!WhetherToNotWarnIfMissingEther.state && Hierarchy.allYullAndEnabledAndUnique<Ether>().isEmpty())
+		{
+			"There is no Ether in the hierarchy, so auto behaviour updating will not be called, and some methods will not work.\n(this warning can be disabled via the menubar)".print();
+		}
+	}
+	private static void potentiallyWarn(PlayModeStateChange playModeStateChange)
+		=> potentiallyWarn();
+}

2d396c630807202f6b532e6f27b0809645cf0203
Hunter-Bobeck <hdbipod@gmail.com>
2020-01-15 17:58:16 -0500

    v.25.5.3
    • updated headset unwearing handling

--- Assets/Plugins/Moon Motion Toolkit/Unity Editor/PausingWhenHeadsetIsUnwearing.cs
+++ Assets/Plugins/Moon Motion Toolkit/Unity Editor/PausingWhenHeadsetIsUnwearing.cs
@@ -5,45 +5,47 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-// Playing When Headset Is Unworn:
-// • according to a menubar toggle, does not \does pause when the headset is unworn
+// Pausing When Headset Is Unwearing:
+// • according to a menubar toggle, pauses when the headset is unwearing
+// • gives Tabbing To Scene When Headset Is Unworn a chance to execute first
 [InitializeOnLoad]
-public static class PausingWhenHeadsetIsUnworn
+public static class PausingWhenHeadsetIsUnwearing
 {
 	#region event handling
-
-	static PausingWhenHeadsetIsUnworn()
+	
+	static PausingWhenHeadsetIsUnwearing()
 	{
 		EditorApplication.update += potentiallyPauseForHeadsetUnwearing;
 	}
-
+	
 	private static bool headsetApparentlyBeingWornPreviously = false;
-
+	
 	private static void potentiallyPauseForHeadsetUnwearing()
 	{
 		if (UnityIs.inEditorPlayMode)
 		{
-			if (!state)
+			if (state)
 			{
 				if (headsetApparentlyBeingWornPreviously && Headset.isApparentlyNotBeingWorn)
 				{
-					TabbingToSceneWhenHeadsetIsUnworn.potentiallyTabToSceneForHeadsetUnwearing();		// give Tabbing To Scene When Headset Is Unworn a chance to execute first
+					// pause, but give Tabbing To Scene When Headset Is Unworn a chance to execute first //
+					TabbingToSceneWhenHeadsetIsUnwearing.potentiallyTabToSceneForHeadsetUnwearing();
 					Pause.editor();
 				}
 			}
-
+			
 			headsetApparentlyBeingWornPreviously = Headset.isApparentlyBeingWorn;
 		}
 	}
 	#endregion event handling
-
-
+	
+	
 	#region menubar toggle
-
-	private const string menuItem = "Pause/Do Not Pause When Headset Is Unwearing";
-
+	
+	private const string menuItem = "Pause/When Headset Is Unwearing";
+	
 	public static bool state => Menu.GetChecked(menuItem);
-
+	
 	[MenuItem(menuItem)]
 	public static void toggleWhetherToNotPauseWhenHeadsetIsUnwearing()
 		=> Menu.SetChecked(menuItem, !state);
--- Assets/Plugins/Moon Motion Toolkit/Unity Editor/TabbingToSceneWhenHeadsetIsUnwearing.cs
+++ Assets/Plugins/Moon Motion Toolkit/Unity Editor/TabbingToSceneWhenHeadsetIsUnwearing.cs
@@ -4,20 +4,21 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-// Tabbing To Scene When Headset Is Unworn:
+// Tabbing To Scene When Headset Is Unwearing:
 // • according to a menubar toggle, does not \does tab to the Scene window when the headset is unworn
+// • Pausing When Headset Is Unwearing gives a chance to potentially tab to scene for headset unwearing before pausing
 [InitializeOnLoad]
-public static class TabbingToSceneWhenHeadsetIsUnworn
+public static class TabbingToSceneWhenHeadsetIsUnwearing
 {
 	#region event handling
-
-	static TabbingToSceneWhenHeadsetIsUnworn()
+	
+	static TabbingToSceneWhenHeadsetIsUnwearing()
 	{
 		EditorApplication.update += potentiallyTabToSceneForHeadsetUnwearing;
 	}
-
+	
 	private static bool headsetApparentlyBeingWornPreviously = false;
-
+	
 	public static void potentiallyTabToSceneForHeadsetUnwearing()
 	{
 		if (!state)
@@ -31,16 +32,16 @@ public static class TabbingToSceneWhenHeadsetIsUnworn
 		headsetApparentlyBeingWornPreviously = Headset.isApparentlyBeingWorn;
 	}
 	#endregion event handling
-
-
+	
+	
 	#region menubar toggle
 
-	private const string menuItem = "Headset Tabbing/Do Not Tab To Scene When Headset Is Unworn";
+	private const string menuItem = "Headset Tabbing/Do Not Tab To Scene When Headset Is Unwearing";
 
 	public static bool state => Menu.GetChecked(menuItem);
 
 	[MenuItem(menuItem)]
-	public static void toggleWhetherToNotTabToSceneWhenHeadsetIsUnworn()
+	public static void toggleWhetherToNotTabToSceneWhenHeadsetIsUnwearing()
 		=> Menu.SetChecked(menuItem, !state);
 	#endregion menubar toggle
 }

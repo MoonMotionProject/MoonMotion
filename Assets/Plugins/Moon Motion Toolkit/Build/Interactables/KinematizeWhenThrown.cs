156ce1971a5e5e7fa19e98acb2dd51df7f782278
Hunter-Bobeck <hdbipod@gmail.com>
2020-02-26 16:26:47 -0500

    v.30.0.6
    • updated Kinematize When Thrown

--- Assets/Plugins/Moon Motion Toolkit/Build/Interactables/KinematizeWhenThrown.cs
+++ Assets/Plugins/Moon Motion Toolkit/Build/Interactables/KinematizeWhenThrown.cs
@@ -7,13 +7,15 @@ namespace Valve.VR.InteractionSystem
 {
 	[RequireComponent(typeof(Interactable))]
 	[RequireComponent(typeof(Rigidbody))]
+	[RequireComponent(typeof(Throwable))]
 	public class KinematizeWhenThrown : AutoBehaviour<KinematizeWhenThrown>
 	{
-		// updating //
-
+		#region updating
+		
 		
 		// Interactable events //
 		private void OnDetachedFromHand(Hand hand)        // "Called every Update() while this GameObject is attached to the hand"
 			=> kinematize();
+		#endregion updating
 	}
 }
